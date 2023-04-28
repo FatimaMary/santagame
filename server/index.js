5fb86994fccfa6d8fe7e79fd7af92c271b334ba1
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-04-28 14:11:37 +0530

    Backend file created

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import userRoutes from './routes/user.js';
+import playGroupRoutes from './routes/playgroup.js';
+import groupPlayersRoutes from './routes/groupplayers.js';
+import wishlistRoutes from './routes/wishlist.js';
+import productRoutes from './routes/products.js';
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+app.use("/user", userRoutes);
+app.use("/playgroup", playGroupRoutes);
+app.use("/groupplayers", groupPlayersRoutes);
+app.use("/wishlist", wishlistRoutes);
+app.use("/products", productRoutes);
