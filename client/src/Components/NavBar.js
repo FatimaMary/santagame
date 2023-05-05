5cbfa94aa852d534631ce919cc7b7cd17dec49fa
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-05-05 16:30:03 +0530

    Nav bar corrected

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Icon } from "react-icons-kit";
 import { menu } from "react-icons-kit/feather/menu";
+// import { Link } from "react-router-dom";
 import { x } from "react-icons-kit/feather/x";
-import '../App.css';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import "../App.css";
 
+export const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
 
-function NavBar() {
-    const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    const handleToggle = () => {
-      setToggle(!toggle);
-    };
-  
-    return (
-      <nav className={toggle ? "navbar expanded" : "navbar"}>
-        <div className="logo"></div>
-        <div className="toggle-icon" onClick={handleToggle}>
-          {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
-        </div>
-        <ListItem className="links" >
-          <ListItemText>My Wishlist</ListItemText>
-          <ListItemText>Secret Santa</ListItemText>
-          <ListItemText>
-            Login
-            {/* <Link to="/Login">Login</Link> */}
-          </ListItemText>
-          <ListItemText>Help</ListItemText>
-        </ListItem>
-      </nav>
-    );
-  
-}
+  return (
+    <nav className={toggle ? "navbar expanded" : "navbar"}>
+      <div className="logo"></div>
+      <div className="toggle-icon" onClick={handleToggle}>
+        {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
+      </div>
+      <ul className="links">
+        <li>My Wishlist</li>
+        <li>Secret Santa</li>
+        <li>
+          Login
+          {/* <Link to="/Login">Login</Link> */}
+        </li>
+        <li>Help</li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
