3a0d7cb027f05fd2102fcaa0b19a1b316116cd06
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-05-22 14:50:10 +0530

    images updated

--- client/src/scenes/slider/slidesData.jsx
+++ client/src/scenes/slider/slidesData.jsx
@@ -1,7 +1,15 @@
 import image1 from "./Imgs/img1.png";
 import image2 from "./Imgs/img2.png";
 import image3 from "./Imgs/img3.png";
-import image4 from "./Imgs/img4.jfif";
+import image4 from "./Imgs/img4.png";
+import image5 from "./Imgs/img5.png";
+import image6 from "./Imgs/img6.png";
+import image7 from "./Imgs/img7.png";
+import image8 from "./Imgs/img8.png";
+import image9 from "./Imgs/img9.png";
+import image10 from "./Imgs/img10.png";
+import image11 from "./Imgs/img11.png";
+
 
 export const slidesData = [
     {
@@ -30,5 +38,47 @@ export const slidesData = [
       image: image4,
       alt: 'Alt text for image 4',
       text: 'christmas theme'
-    }
+    },
+    {
+      id: 5,
+      image: image5,
+      alt: 'Alt text for image 5',
+      text: 'christmas theme'
+    },
+    {
+      id: 6,
+      image: image6,
+      alt: 'Alt text for image 6',
+      text: 'christmas theme'
+    },
+    {
+      id: 7,
+      image: image7,
+      alt: 'Alt text for image 7',
+      text: 'christmas theme'
+    },
+    {
+      id: 8,
+      image: image8,
+      alt: 'Alt text for image 8',
+      text: 'christmas theme'
+    },
+    {
+      id: 9,
+      image: image9,
+      alt: 'Alt text for image 9',
+      text: 'christmas theme'
+    },
+    {
+      id: 10,
+      image: image10,
+      alt: 'Alt text for image 10',
+      text: 'christmas theme'
+    },
+    {
+      id: 11,
+      image: image11,
+      alt: 'Alt text for image 11',
+      text: 'christmas theme'
+    },
   ];
