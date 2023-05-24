1d7ba972fe94790a1a831d3d2a87463d6e4805ce
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-05-24 16:30:31 +0530

    Update index.jsx

--- client/src/scenes/wishlist/index.jsx
+++ client/src/scenes/wishlist/index.jsx
@@ -7,6 +7,14 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
 function Wishlist() {
+const Ages = [];
+
+for (let i = 8; i <= 115; i++) {
+  Ages.push({ value: String(i), label: String(i) });
+}
+
+console.log(Ages);
+
   return <Box m='1.5rem 2.5rem'
     display='flex'
     flexDirection='column'
@@ -41,25 +49,27 @@ function Wishlist() {
     <Box>
     <Typography>Age</Typography>
     <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="demo-select-small-label">Age</InputLabel>
+      {/* <InputLabel id="demo-select-small-label">Age</InputLabel> */}
       <Select
         labelId="demo-select-small-label"
         id="demo-select-small"
         // value={age}
         label="Age"
         // onChange={handleChange}
+        sx={{
+            width: '200px'
+        }}
       >
-        <MenuItem value="">
-          <em>None</em>
-        </MenuItem>
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {Ages.map((options) => (
+            <MenuItem key={options.value} value={options.value}>
+                {options.label} years old
+            </MenuItem>
+        ))}
       </Select>
     </FormControl>
     </Box>
     <Box>
-        <Button>Continue</Button>
+        <Button variant='contained'>Continue</Button>
     </Box>
   </Box>
 }
